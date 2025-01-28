e960f1fe3d045ce5221010a57891c3d4204ee4b5
Ryan Deist <ryandeist@gmail.com>
2025-01-27 19:33:54 -0500

    Greasy Pizza

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 const App = () => {
-<<<<<<< HEAD
   return (
   <>
     <h1>Greasy Pizza Box Media Match!</h1>
     <p>Hi my name is Ryan I like cheeseburgers.</p>
   </>
-
-
-
   );
-=======
-  return <h1>Empty Pizza Box Media Match!</h1>;
-
->>>>>>> b7bac3b109336cfd13e2adb4bc38c511d4a2f4d3
 };
 
 export default App
